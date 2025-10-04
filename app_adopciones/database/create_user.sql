396f51332d5dfab3f903697db65b2ba58f4a7ac2
diego5714 <diego5714@proton.me>
2025-10-04 04:21:31 -0300

    Creados scripts de sql para crear usuario y darle permisos sobre la base de datos.

---
+++ app_adopciones/database/create_user.sql
@@ -0,0 +1,3 @@
+-- Active: 1759466597112@@127.0.0.1@3306@tarea2
+-- Crear usuario
+CREATE USER 'cc5002'@'localhost' IDENTIFIED BY 'programacionweb';
---
+++ app_adopciones/database/grant_user_permissions.sql
@@ -0,0 +1,3 @@
+-- Active: 1759466597112@@127.0.0.1@3306@tarea2
+-- Darle permisos al usuario
+GRANT ALL ON tarea2.* TO cc5002@localhost;
