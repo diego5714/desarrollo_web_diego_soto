2a12c23becdf2a181309315154581c309bdf2097
diego5714 <diego5714@proton.me>
2025-11-17 22:57:36 -0300

    Correccion del script de sql que crea el usuario de la app

--- app_adopciones/database/create_java_user.sql
+++ app_adopciones/database/create_java_user.sql
@@ -1,5 +1,5 @@
 CREATE USER 'java_app_user'@'127.0.0.1' IDENTIFIED BY 'programacionweb';
-GRANT SELECT ON tarea2.aviso_adopcion TO 'java_app_user'@'localhost';
-GRANT SELECT ON tarea2.comuna TO 'java_app_user'@'localhost';
-GRANT SELECT ON tarea2.region TO 'java_app_user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON tarea2.nota TO 'java_app_user'@'localhost';
+GRANT SELECT ON tarea2.aviso_adopcion TO 'java_app_user'@'127.0.0.1';
+GRANT SELECT ON tarea2.comuna TO 'java_app_user'@'127.0.0.1';
+GRANT SELECT ON tarea2.region TO 'java_app_user'@'127.0.0.1';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tarea2.nota TO 'java_app_user'@'127.0.0.1';
